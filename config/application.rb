9c0331ac94344483c72222a6b3b54c5ac681937c
prudepixie <wndyhsu@gmail.com>
2016-08-18 15:12:48 -0700

    add first angular module

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module WhiskersBnb2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass","assets","fonts")
+
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
   end
 end
